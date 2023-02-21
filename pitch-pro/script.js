2a441af798e4ae79ea057107e46047592057db20
Yuvaraj-2001 <yuvayuvaraj720444@gmail.com>
2023-02-21 23:15:34 +0530

    google analytics first Event

--- pitch-pro/script.js
+++ pitch-pro/script.js
@@ -119,9 +119,15 @@ function dsdasd(){
 }
 
 window.dataLayer = window.dataLayer || [];
-function gtag(){
-  dataLayer.push(arguments);
-}
 gtag('js', new Date());
-
 gtag('config', 'G-NQZF99DXD2');
+
+function onLoadDataLayer(){
+  dataLayer.push({
+    'color': 'red',
+    'time': +new Date(),
+    'conversionValue': 50,
+    'event': 'User Load'
+  });
+}
+onLoadDataLayer();
